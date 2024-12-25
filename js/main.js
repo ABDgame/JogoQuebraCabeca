36e1234760fd8858104d67494b29fe7c74c0c193
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-25 16:18:41 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+/* header bg */
+window.addEventListener("scroll", function() {
+  if(this.scrollY > 0){
+    document.querySelector(".header").classList.add("header-bg");
+  }
+  else{
+    document.querySelector(".header").classList.remove("header-bg");
+  }
+});
